e2fccdac6c61d6f8df9c761d32c754e98a088e5c
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-05 21:37:54 -0700

    Add Player class

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,6 @@
 class Board
-  attr_reader: state
+  attr_reader :state
+
   def initialize
     @state = Array.new(3, Array.new(3))
   end
@@ -11,3 +12,22 @@ class Board
     @state[x_loc][y_loc] = marker
   end
 end
+
+
+class Player
+  attr_accessor :name, :player_marker, :score
+
+  def initialize(name, player_marker, wins)
+    @name = name
+    @player_marker = player_marker
+    @wins = 0
+  end
+
+  # Create a new player, either player 1 or player 2 (default if 1 not passed)
+  # 
+  # @return [Player]: A new Player
+  def self.create_player(name, player_number)
+    player_number == 1 ? Player.new(name, 'x', 0) : Player.new(name, 'o', 0)
+  end
+end
+

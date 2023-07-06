c7c85f85ae12c13c784d5f53230e3244cc145640
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-05 23:22:27 -0700

    Add get player move

--- tictactoe.rb
+++ tictactoe.rb
@@ -11,6 +11,22 @@ class Board
   def update_cell(marker, x_loc, y_loc)
     @state[x_loc][y_loc] = marker
   end
+
+  # Get all open cells on the board (all valid moves)
+  #
+  # @return [Array]: An array containing all open cell values
+  def open_cells()
+    row_names = ['A', 'B', 'C']
+    row = 0
+
+    open_cells = @state.flatten.each_with_index.reduce([]) do |result, (cell, index)| 
+      row += 1 if index >= 1 && index % 3 == 0
+      cell_number = index % 3 + 1
+      result.append("#{row_names[row]}#{cell_number}")
+      result
+    end
+  end
+ 
 end
 
 
@@ -22,6 +38,7 @@ class Player
     @player_marker = player_marker
     @wins = 0
   end
+
 end
 
 
@@ -43,19 +60,30 @@ class Game
   end
 
   private
+
   def game_loop()
     game_end = false
     until game_end
       display_board()
+      player_move = get_player_move(@turn, @board.open_cells)
       break
     end
   end
 
-  private
+
   def display_board()
     @board.state.each do |row|
       puts row.map { |cell| cell.empty? ? ' ' : cell }.join(' | ')
       puts '-' * (row.length * 3) unless row.object_id == @board.state.last.object_id
+    end
+  end
+
+
+  def get_player_move(player, available_moves)
+    move = ""
+    until available_moves.include?(move.upcase)
+      puts "Please enter a valid cell from #{available_moves.join(", ")}!"
+      move = gets.chomp()
     end
   end
 end
